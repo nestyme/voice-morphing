49b475e0af40f7d86188cee9c672fca8f3b11860
Nadya Zueva <zueva.nn@phystech.edu>
2020-10-09 19:19:51 +0300

    update for inference age_regressor

--- age_regressor/run_model.py
+++ age_regressor/run_model.py
@@ -1,9 +1,8 @@
 import torch
-from utils.preprocess import timit_dataloader
 from gender_detector.model import Model
 import librosa
-from utils.preprocess import timit_dataloader, dataloader
-from sklearn.metrics import accuracy_score
+import numpy as np
+import torch as tensor
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'using {device} mode')
@@ -11,7 +10,6 @@ model = Model()
 model.load_state_dict(
     torch.load(
         "age_regressor/checkpoint/best.pt", map_location={'cuda:0': 'cpu'}))
-
 if device == torch.device('cuda'):
     model.cuda()
 else:
@@ -19,29 +17,30 @@ else:
 model.eval()
 
 
+def spec_to_image(spec, eps=1e-6):
+        mean = spec.mean()
+        std = spec.std()
+        spec_norm = (spec - mean) / (std + eps)
+        spec_min, spec_max = spec_norm.min(), spec_norm.max()
+        spec_scaled = 255 * (spec_norm - spec_min) / (spec_max - spec_min)
+        spec_scaled = spec_scaled.astype(np.uint8)
+        return spec_scaled
+
+def preprocess_sample_inference(amplitudes, sr=16000, max_length=150, device='cpu'):
+    spectrogram = librosa.feature.melspectrogram(amplitudes, sr=sr, n_mels=128, fmin=1, fmax=8192)[:, :max_length]
+    spectrogram = np.pad(spectrogram, [[0, 0], [0, max(0, max_length - spectrogram.shape[1])]], mode='constant')
+    spectrogram = np.array([spec_to_image(np.float32(spectrogram))]).transpose([0, 2, 1])
+
+    return torch.tensor(spectrogram, dtype=torch.float).to(device, non_blocking=True)
 def predict(wavfile):
-    _timit_dataloader = timit_dataloader(train_mode=False)
     waveform, _ = librosa.load(wavfile, sr=16000)
 
-    input = _timit_dataloader.preprocess_sample_inference(waveform)
+    input = preprocess_sample_inference(waveform)
     with torch.no_grad():
         out = model(input).cpu().detach().numpy()
-    out  = [round(tmp) for tmp in out]
     return out
 
 
 if __name__ == '__main__':
-    _timit_dataloader = timit_dataloader(data_path='./data/TIMIT', train_mode=False, age_mode=True)
-    test = _timit_dataloader.return_test()
-    # print(predict('data/female.wav'))
-    # print(predict('data/male.wav'))
-    testset = dataloader(*test)
-    model.eval()
-
-    with torch.no_grad():
-        input, target = testset.next_batch(150, device=device)
-        out = model(input)
-        out, target = out.cpu().detach().numpy(), target.cpu().detach().numpy()
-        out = [round(tmp) for tmp in out]
-        print(out, target)
-        print(f'accuracy_score:{accuracy_score(out, target)}')
+    print(predict('data/female.wav'))
+    print(predict('data/male.wav'))
